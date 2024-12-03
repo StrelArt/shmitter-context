4c54253fdf639dcd6e4df79c2417fb090fceb438
StrelArt <streltsyn.artem@gmail.com>
2024-12-03 20:14:14 +0200

    Full Redux

--- shmitter-context/src/actions/userAction.js
+++ shmitter-context/src/actions/userAction.js
@@ -5,3 +5,19 @@ export const changeAvatar = url => ({
         payload: url
     }
 )
+
+export const CHANGE_NAME = 'CHANGE_NAME';
+
+export const changeName = name => ({
+        type: CHANGE_NAME,
+        payload: name
+    }
+)
+
+export const CHANGE_STATS = 'CHANGE_STATS';
+
+export const changeStats = (statsType, sum) => ({
+    type: CHANGE_STATS,
+    payload: {statsType, sum}
+
+})
